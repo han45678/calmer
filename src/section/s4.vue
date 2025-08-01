5ecce965ee1a8241e395cce6bec54e785916046f
han <han840717@hightflight.com>
2025-08-01 18:10:26 +0800

    s4

--- src/section/s4.vue
+++ src/section/s4.vue
@@ -1,11 +1,15 @@
 <template>
   <article class="s4 " ref="s4">
-
+    <img class="bg" src="./s4/bg.png" alt="bg">
   </article>
 </template>
 
 <style lang="scss">
 @import '@/assets/style/function.scss';
 
-.s4 {}
+.s4 {
+  .bg{
+    width: 100%;
+  }
+}
 </style>
