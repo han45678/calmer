02dfb76679ec819602abf5c7060625d8966306a4
xcatx <xcatx796@gmail.com>
2025-08-06 11:38:37 +0800

    改感謝頁

--- src/pages/FormThanks.vue
+++ src/pages/FormThanks.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="thanks">
+    <div class="thanks font-['Noto_Serif_TC',serif]">
         <img src="https://bcp.crwdcntrl.net/5/c=13578/b=78113418" width="1" height="1" />
-        <a href="/">
-            感謝您的預約<br>回首頁
-        </a>
+        
+            <a href="/"><img  src="@/section/s1/logo.svg" class="logo" /> </a><br>
+感謝您的預約，將由專人與您聯繫<br>
+誠摯邀請您蒞臨鑑賞<br><br>
+微笑公園×巨蛋3-4房<br>
+—— 靜謐藏心 城隱雅居<br><br><a href="/">回首頁</a>
     </div>
 </template>
 <style lang="scss" scoped>
@@ -12,14 +15,6 @@
     color: #fff;
     width: 100vw;
     height: 100vh;
-  &::before{content: "";position:fixed;top: 0;left: 0;bottom: 0;right: 0;
-background: url("@/section/form/bg.jpg");
-background-size: cover;z-index: -1;
-
-@media screen and (max-width: 767px) {
-  background-image: url("@/section/form/bgm.jpg");
-}
-}
     /*
     background: url('//h35.banner.tw/img/thank_bg.png');
     background-size: cover;
@@ -30,6 +25,25 @@ background-size: cover;z-index: -1;
     align-items: center;
     justify-content: center;
     text-align: center;
+    gap: 0;
+  &::before{content: "";position:fixed;top: 0;left: 0;bottom: 0;right: 0;
+background: url("@/section/form/bg.jpg");
+background-size: cover;z-index: -1;
+
+@media screen and (max-width: 767px) {
+  background-image: url("@/section/form/bgm.jpg");
+}
+}
+  .logo{
+    width:16vw;
+    margin-bottom: 1.5em;
+  @media screen and (min-width: 768px) {
+    width:4vw;
+  }
+  
+  }
+
+
     a {
         .img {
             position: absolute;
--- src/pages/PhoneThanks.vue
+++ src/pages/PhoneThanks.vue
@@ -1,22 +1,28 @@
 <template>
-    <div class="thanks">
-        <a href="/">
-            感謝您的來電<br>回首頁
-        </a>
+    <div class="thanks font-['Noto_Serif_TC',serif]">
+        <a href="/"><img  src="@/section/s1/logo.svg" class="logo" /> </a><br>
+感謝您的預約，將由專人與您聯繫<br>
+誠摯邀請您蒞臨鑑賞<br><br>
+微笑公園×巨蛋3-4房<br>
+—— 靜謐藏心 城隱雅居<br><br><a href="/">回首頁</a>
     </div>
 </template>
 <style lang="scss" scoped>
 .thanks {
+    color: #fff;
     width: 100vw;
     height: 100vh;
+    /*
+    background: url('//h35.banner.tw/img/thank_bg.png');
+    background-size: cover;
+    background-position: center;
+    */
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     text-align: center;
-    color: #fff;
-    width: 100vw;
-    height: 100vh;
+    gap: 0;
   &::before{content: "";position:fixed;top: 0;left: 0;bottom: 0;right: 0;
 background: url("@/section/form/bg.jpg");
 background-size: cover;z-index: -1;
@@ -25,37 +31,31 @@ background-size: cover;z-index: -1;
   background-image: url("@/section/form/bgm.jpg");
 }
 }
+  .logo{
+    width:16vw;
+    margin-bottom: 1.5em;
+  @media screen and (min-width: 768px) {
+    width:4vw;
+  }
+  
+  }
+
 
     a {
         .img {
             position: absolute;
             opacity: 1;
         }
-
         .img-hover {
             opacity: 0;
         }
-
         &:hover {
             color: #ff0;
             .img {
                 opacity: 0;
             }
-
             .img-hover {
                 opacity: 1;
-            }
-        }
-    }
-}
-
-@media screen and (max-width: 767px) {
-    .thanks {
-        a {
-
-            .img,
-            .img-hover {
-                width: 90vw;
             }
         }
     }
